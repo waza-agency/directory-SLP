3b063e190e81fc1f8393a3189c1e724338cb56f6
Santiago Gonzalez <santiagogonzalez@Mac-mini.local>
2025-05-13 19:03:18 -0600

    fixed issues

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <meta name="google-site-verification" content="s4k5V8dAkvDb3Dv15SNozffS7noII7qQAsUXJfnALOU" />
-        <meta name="google-adsense-account" content="ca-pub-7339948154887436" />
+        <meta name="google-adsense-account" content="ca-pub-2759148102" />
         <link rel="dns-prefetch" href="https://pagead2.googlesyndication.com" />
         <link rel="dns-prefetch" href="https://googleads.g.doubleclick.net" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -13,7 +13,7 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Playfair+Display:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7339948154887436" crossOrigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2759148102" crossOrigin="anonymous"></script>
       </Head>
       <body className="font-sans antialiased text-gray-800 bg-white">
         <Main />
