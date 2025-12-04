48ecb3a701ec1c3971a987aacafd1f675d5bffa4
Santiago Gonzalez <santiagogonzalez@Mac-mini.local>
2025-12-04 16:42:40 -0600

    agente resolvio issues

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,11 +1,16 @@
+const path = require('path');
+
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en', 'zh', 'ja', 'de'],
   },
+  ns: ['common'],
+  defaultNS: 'common',
   fallbackLng: {
     default: ['es'],
   },
+  localePath: path.resolve('./public/locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
